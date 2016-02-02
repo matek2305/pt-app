1bcba6011791bf361006484f160b38a0a8318ad4
Mateusz Urbański <matek2305@gmail.com>
2016-02-02 19:10:20 +0100

    Initial commit.

---
+++ src/main/java/io/github/matek2305/pt/DemoApplication.java
@@ -0,0 +1,12 @@
+package io.github.matek2305.pt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
