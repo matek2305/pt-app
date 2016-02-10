953fc83f645b44d7e6fcbdec2e514716d736069b
Mateusz Urbański <matek2305@gmail.com>
2016-02-10 19:50:25 +0100

    added jackson-datatype-jsr310 for human readable jdk8 datetime serialization

--- src/main/java/io/github/matek2305/pt/api/ApiConfiguration.java
+++ src/main/java/io/github/matek2305/pt/api/ApiConfiguration.java
@@ -2,13 +2,11 @@ package io.github.matek2305.pt.api;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /**
  * @author Mateusz Urbański <matek2305@gmail.com>
  */
 @Configuration
 @ComponentScan
-@EnableWebMvc
 public class ApiConfiguration {
 }
