db96e150219e67aa3001357a314e5afba11754b9
Mateusz Urbański <matek2305@gmail.com>
2016-03-02 20:53:20 +0100

    security config for dev profile

--- src/main/java/io/github/matek2305/pt/api/config/DevSecurityConfiguration.java
+++ src/main/java/io/github/matek2305/pt/api/config/DevSecurityConfiguration.java
@@ -1,6 +1,7 @@
 package io.github.matek2305.pt.api.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -10,10 +11,16 @@ import static com.stormpath.spring.config.StormpathWebSecurityConfigurer.stormpa
  * @author Mateusz Urbański <matek2305@gmail.com>.
  */
 @Configuration
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+@Profile("dev")
+public class DevSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.apply(stormpath());
+        http
+                .apply(stormpath()).and()
+                .authorizeRequests()
+                    .antMatchers("/**").permitAll().and()
+                .headers()
+                    .frameOptions().disable();
     }
 }
---
+++ src/test/groovy/io/github/matek2305/pt/api/MatchesControllerSelfITSpec.groovy
@@ -0,0 +1,65 @@
+package io.github.matek2305.pt.api
+
+import info.solidsoft.mockito.java8.api.WithBDDMockito
+import io.github.matek2305.pt.api.config.ApiConfiguration
+import io.github.matek2305.pt.domain.entity.Match
+import io.github.matek2305.pt.domain.entity.Tournament
+import io.github.matek2305.pt.service.MatchService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.SpringApplicationContextLoader
+import org.springframework.boot.test.WebIntegrationTest
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.web.context.WebApplicationContext
+import spock.lang.Specification
+import spock.lang.Unroll
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup
+/**
+ * @author Mateusz Urbański <matek2305@gmail.com>.
+ */
+@WebIntegrationTest
+@ContextConfiguration(loader = SpringApplicationContextLoader, classes = [ApiConfiguration, ServiceMockConfiguration])
+class MatchesControllerSelfITSpec extends Specification implements WithBDDMockito {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext
+
+    @Autowired
+    private MatchService matchService
+
+    @Autowired
+    private MatchesController matchesController
+
+    private MockMvc mockMvc
+
+    void setup() {
+        mockMvc = webAppContextSetup(webApplicationContext).build()
+    }
+
+    @Unroll
+    def "should have predictions link when status is #status" (Match.Status status) {
+        given:
+            def matchId = 12
+        and:
+            given(matchService.getMatch(matchId))
+                    .willReturn(Optional.of(new Match(id: matchId, status: status, tournament: new Tournament(id: 1))))
+        expect:
+            mockMvc.perform(get("/matches/${matchId}")).andExpect(jsonPath('$._links.predictions').exists())
+        where:
+            status << [Match.Status.PREDICTION_CLOSED, Match.Status.STARTED, Match.Status.FINISHED, Match.Status.CANCELED]
+    }
+
+    def "should not have predictions link when status is PREDICTIONS_AVAILABLE" () {
+        given:
+            def matchId = 12
+        and:
+            given(matchService.getMatch(matchId))
+                    .willReturn(Optional.of(new Match(id: matchId, status: Match.Status.PREDICTION_AVAILABLE, tournament: new Tournament(id: 1))))
+        expect:
+            mockMvc.perform(get("/matches/${matchId}")).andExpect(jsonPath('$._links.predictions').doesNotExist())
+    }
+
+}
