08714726b4ad9c0223a970551b3b469e341e2923
Mateusz Urbański <matek2305@gmail.com>
2016-03-06 17:20:59 +0100

    controllers spec fix

--- src/test/java/com/github/matek2305/pt/api/ControllerITSpec.java
+++ src/test/java/com/github/matek2305/pt/api/ControllerITSpec.java
@@ -1,6 +1,7 @@
 package com.github.matek2305.pt.api;
 
 import com.github.matek2305.pt.api.config.ApiConfiguration;
+import com.github.matek2305.pt.auth.DevAuthenticationFacadeImpl;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
@@ -8,16 +9,18 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import org.springframework.boot.test.SpringApplicationContextLoader;
 import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 
 /**
  * @author Mateusz Urbański <matek2305@gmail.com>
  */
 @Inherited
+@ActiveProfiles("dev")
 @WebIntegrationTest
 @ContextConfiguration(
         loader = SpringApplicationContextLoader.class,
-        classes = { ApiConfiguration.class, ServiceMockConfiguration.class }
+        classes = { ApiConfiguration.class, ServiceMockConfiguration.class, DevAuthenticationFacadeImpl.class }
 )
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
